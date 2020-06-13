18c743afcbb26a72ab68247078a5cc3b41fd8dde
Yung Cher Ho <ycho527@hotmail.com>
2020-06-13 22:58:36 +0800

    Edited readme file

--- src/app/components/categories/Category.js
+++ src/app/components/categories/Category.js
@@ -1,1 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from '../navbar/Navbar';
+import classes from './Category.module.css'
 
+const Category = (props) => {
+
+  return (
+    <div></div>
+  )
+
+
+}
+
+export default Category
